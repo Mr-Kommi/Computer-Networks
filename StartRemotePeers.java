81a1fce52442ed42d5e13d49abee59ecf1ae9430
Mr-Kommi <147011967+Mr-Kommi@users.noreply.github.com>
2024-03-27 14:29:34 -0700

    Added files
    Uploaded project

---
+++ StartRemotePeers.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//import bittorrent.ops.PropertiesEnum;
+
+public class StartRemotePeers {
+
+    public static void main(String[] args) {
+
+        String path = "Computer-Networks/";
+
+        ArrayList<PeerInfo> peerList = new ArrayList<>();
+        Scanner sc = null;
+        int seq = 1;
+        try {
+            sc = new Scanner(new File(System.getProperty("user.dir") + File.separatorChar
+			+ "PeerInfo.cfg"));
+            while (sc.hasNextLine()) {
+                String arr[] = sc.nextLine().split(" ");
+                if (arr.length > 0) {
+                    peerList.add(new PeerInfo(arr[0], arr[1]));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("PeerInfo.cfg missing");
+        }
+        finally {
+            sc.close();
+        }
+
+        for (PeerInfo pInfo : peerList) {
+            try {
+                Thread.sleep(1000);
+                
+                Runtime.getRuntime().exec("ssh -i .ssh/id_rsa gopichan@" + pInfo.getHostName() + " cd " + path +
+                        "; java peerProcess " + pInfo.getPeerID() + " > " + pInfo.peerID + "console.log");
+                System.out.println("Start remote peer " + pInfo.getPeerID() + " at " + pInfo.getHostName());
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class PeerInfo {
+
+        private String peerID;
+        private String hostName;
+
+        public PeerInfo(String peerID, String hostName) {
+            super();
+            this.peerID = peerID;
+            this.hostName = hostName;
+        }
+
+        public String getPeerID() {
+            return peerID;
+        }
+
+        public String getHostName() {
+            return hostName;
+        }
+
+    }
+
+}
---
+++ peerProcess.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+
+import bittorrent.executor.*;
+
+public class peerProcess {
+
+	private static boolean simulate = false;
+
+	public static void main (String args[]) {
+
+		String[] peers = new String[] {"1001", "1002", "1003"};
+		PeerSharingInit peerProcessExecutor = null;
+
+		for (String arg: args){
+			if (arg.equals("-s")){
+				simulate = true;
+			}
+		}
+
+		// Starting the peer Connections
+
+		if (simulate) {
+			for (String peerId: peers) {
+				Thread t = new Thread(new PeerSharingInit(peerId));
+				t.start();
+			}
+		}
+		else {
+			if (args.length > 0) {
+
+				//Fetching peerId from arguments
+				peerProcessExecutor = new PeerSharingInit(args[0]);
+
+			} else {
+
+				//default value
+				peerProcessExecutor = new PeerSharingInit("1001");
+			}
+			peerProcessExecutor.init();
+		}
+	}
+}
